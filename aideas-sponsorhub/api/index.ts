4143466543de42d483af03de548b8cdd0c916bac
sohmh <esmvee2006@gmail.com>
2026-09-20 15:01:45 +0530

    Test Vercel API function

--- aideas-sponsorhub/api/index.ts
+++ aideas-sponsorhub/api/index.ts
@@ -1,5 +1,6 @@
-import { createApp } from "../server/_core/app.js";
-
-const app = createApp();
-
-export default app;
+export default function handler(req: any, res: any) {
+  res.status(200).json({
+    ok: true,
+    message: "Vercel API function is working",
+  });
+}
